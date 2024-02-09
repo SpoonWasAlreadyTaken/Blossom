80ad11464ef540f3f43fd85a8c1227d96215f289
Spoon <laserpyoro@gmail.com>
2024-02-09 21:28:21 +0200

    Movement 2
    Fixed some movement options, added QOL coyote time and jump buffering.

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -11,6 +11,8 @@ public class Movement : MonoBehaviour
     //Jump customization values
     [SerializeField] private float jumpHeight = 5f;
     [SerializeField] private bool doubleJump = false;
+    [SerializeField] private float coyoteTime = 0.2f; //time you can jump after being off the ground
+    [SerializeField] private float jumpBuffer = 0.3f; //time that your jumps can be queued up
 
 
     [SerializeField] private Transform groundCheck;
@@ -20,48 +22,68 @@ public class Movement : MonoBehaviour
     private bool jumped = true;
     private bool isFacingRight = true;
     private float horizontalSpeed;
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
 
 
     private void Update()
     {
         horizontalSpeed = Input.GetAxisRaw("Horizontal");
 
+        if (IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
 
         if (Input.GetKeyDown("w"))
         {
-            Jump();
+            jumpBufferCounter = jumpBuffer;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
         }
 
-        if (IsGrounded() && doubleJump)
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0 || !jumped) //makes the player jump
+        {
+            jumped = true;
+            player.velocity = new Vector2(player.velocity.x, jumpHeight);
+
+            jumpBufferCounter = 0f;
+        }
+
+        if (Input.GetKeyUp("w") && player.velocity.y > 0f) //makes it so that the player can stop a bit of their jump height if they let go of the jump key
+        {
+            player.velocity = new Vector2(player.velocity.x, player.velocity.y * .7f);
+
+            coyoteTimeCounter = 0f;
+        }
+
+        if (IsGrounded() && doubleJump) //allows the player to double jump if enable
         {
             jumped = false;
         }
 
-        Flip();
-
+        Flip(); 
     }
 
+    //handles the players movement at a fixed time so it can't move faster or slower depending on the FPS
     void FixedUpdate()
     {
         player.velocity = new Vector2(horizontalSpeed * movementSpeed, player.velocity.y);
     }
 
-   private void Jump()
-    {
-        if (IsGrounded() || !jumped)
-        {
-            jumped = true;
-            player.velocity = new Vector2(player.velocity.x, jumpHeight);
-
-            Debug.Log("Jumped");
-        }
-    }
-
+    //determined if the player is on the ground
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     }
 
+    //flips the player in the direction its moveing
     private void Flip()
     {
         if (isFacingRight && horizontalSpeed < 0f || !isFacingRight && horizontalSpeed > 0f)
