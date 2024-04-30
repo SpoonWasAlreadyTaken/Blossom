ac09218c9fdcfe508a6f5a0019d9e35dd37fced1
evasp-00 <evasintijapetersone@gmail.com>
2024-04-30 15:01:07 +0300

    some stuff
    I will jeet myself of some place very high

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class HealthBar : MonoBehaviour
 {
     public PlayerHealth player;
     public Movement movement;
     [SerializeField] private Slider sliderHealth;
     [SerializeField] private Slider sliderStamina;
+
+
 
     void FixedUpdate()
     {
